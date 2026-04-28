7d6d5089b0e498b66acc7655eed89e510633e6c8
Bfilahi <abdelbassete.filahi@gmail.com>
2026-04-28 19:47:24 +0200

    Tested guards: auth-guard, public-guard

--- src/app/guards/public-guard.spec.ts
+++ src/app/guards/public-guard.spec.ts
@@ -1,17 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 
 import { publicGuard } from './public-guard';
+import { AuthService } from '../services/auth-service';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('publicGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
       TestBed.runInInjectionContext(() => publicGuard(...guardParameters));
 
+    let mockAuthService: jasmine.SpyObj<AuthService>;
+    let router: Router;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockAuthService = jasmine.createSpyObj(['isLoggedIn']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideZonelessChangeDetection(),
+        { provide: AuthService, useValue: mockAuthService }
+      ]
+    });
+
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+    it('should not invoke createUrlTree if isLoggedIn is false', () => {
+    mockAuthService.isLoggedIn.and.returnValue(false);
+    spyOn(router, 'createUrlTree');
+
+    executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+
+    expect(router.createUrlTree).not.toHaveBeenCalled();
+  });
+  
+  it('should invoke createUrlTree if isLoggedIn is true', () => {
+    mockAuthService.isLoggedIn.and.returnValue(true);
+    spyOn(router, 'createUrlTree');
+
+    executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot);
+
+    expect(router.createUrlTree).toHaveBeenCalledWith(['/dashboard']);
   });
 });
